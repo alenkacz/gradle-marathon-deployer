89e48b609ec1a9cb7f8cc3f691a71258fbec0ab1
Alena Varkockova <varkockova.a@gmail.com>
2016-08-14 18:45:45 +0200

    More tests, code cleanup

--- src/main/groovy/cz/alenkacz/gradle/marathon/deploy/DeployTask.groovy
+++ src/main/groovy/cz/alenkacz/gradle/marathon/deploy/DeployTask.groovy
@@ -1,41 +1,93 @@
 package cz.alenkacz.gradle.marathon.deploy
 
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
+import org.asynchttpclient.AsyncHttpClient
+import org.asynchttpclient.DefaultAsyncHttpClient
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
+
 class DeployTask extends DefaultTask {
     def PluginExtension pluginExtension
+    def AsyncHttpClient asyncHttpClient
+    def JsonSlurper jsonSlurper
 
     public DeployTask() {
         group = 'publishing'
         description = 'Deploys your application to Marathon'
+
+        asyncHttpClient = new DefaultAsyncHttpClient()
+        jsonSlurper = new JsonSlurper()
     }
     @TaskAction
     def deployToMarathon() {
-        project.exec { execSpec ->
-            if (!pluginExtension.url) {
-                throw new Exception("Missing required property marathon url")
-            }
-
-            if (!pluginExtension.pathToJsonFile || !new File(pluginExtension.pathToJsonFile).exists()) {
-                throw new Exception("Invalid path to marathon json ${pluginExtension.pathToJsonFile}")
-            }
-
-            def jsonAbsolutePath = new File(pluginExtension.pathToJsonFile).absolutePath
-            ArrayList<Object> dockerRunCmd = []
-
-            dockerRunCmd.addAll(['docker', 'run', '-v', "$jsonAbsolutePath:/marathon.json",
-                                 '-e', "MARATHON_URL=${pluginExtension.url}"])
-
-            if (pluginExtension.dockerImageName) {
-                dockerRunCmd.addAll(['-e', "DOCKER_IMAGE_NAME=${pluginExtension.dockerImageName}"])
-            }
-
-            dockerRunCmd << 'avastsoftware/marathon-deployer:latest'
-
-            execSpec.commandLine 'docker', 'pull', '-a', 'avastsoftware/marathon-deployer:latest'
-
-            execSpec.commandLine dockerRunCmd.toArray()
+        if (!pluginExtension.url) {
+            throw new Exception("Missing required property marathon url")
         }
+
+        if (!pluginExtension.pathToJsonFile || !new File(pluginExtension.pathToJsonFile).exists()) {
+            throw new Exception("Invalid path to marathon json ${pluginExtension.pathToJsonFile}")
+        }
+
+        def marathonApiUrl = "${pluginExtension.url}/v2"
+        def parsedJson = jsonSlurper.parse(new File(pluginExtension.pathToJsonFile))
+        def String applicationId = parsedJson.id
+        def marathonJson = getFinalMarathonJson(parsedJson)
+        try {
+            def result = asyncHttpClient.preparePut("${marathonApiUrl}/apps/${java.net.URLEncoder.encode(applicationId, StandardCharsets.UTF_8.toString())}").setBody(marathonJson).execute().get(pluginExtension.deploymentRequestTimeout.toMilliseconds(), TimeUnit.MILLISECONDS)
+            if (result.statusCode != 200 && result.statusCode != 201) {
+                throw new MarathonDeployerException("Marathon responded with code ${result.statusCode} when requesting deployment. Response: ${result.getResponseBody(StandardCharsets.UTF_8)}")
+            }
+        } catch (Exception e) {
+            throw new MarathonDeployerException("Error when requesting to deploy application to Marathon", e)
+        }
+
+        def int notFinishedDeploymentsCount = verifyDeploymentFinished(marathonApiUrl, applicationId)
+        if (notFinishedDeploymentsCount > 0) {
+            throw new MarathonDeployerException("Unable to verify deployment. There are still $notFinishedDeploymentsCount deployments of this application not finished")
+        } else {
+            println("Deployment was successful")
+        }
+    }
+
+    Object getFinalMarathonJson(Object parsedJson) {
+        if (pluginExtension.dockerImageName) {
+            parsedJson.container.docker.image = pluginExtension.dockerImageName
+        }
+        return JsonOutput.toJson(parsedJson)
+    }
+
+    private def int verifyDeploymentFinished(String marathonApiUrl, String applicationId) {
+        def marathonDeploymentUrl = "$marathonApiUrl/deployments"
+        def currentNumberOfDeployments = 666
+        def startTime = new Date()
+        while (currentNumberOfDeployments > 0 && !timedOut(pluginExtension.verificationTimeout, startTime, new Date())) {
+            try {
+                currentNumberOfDeployments = getNumberOfDeployments(marathonDeploymentUrl, applicationId)
+            } catch (Exception e) {
+                // intentionally nothing
+                logger.warn("Unable to receive deployment information when validating deployment")
+            }
+            logger.debug("Number of deployments after verification: $currentNumberOfDeployments")
+            sleep(500)
+        }
+        return currentNumberOfDeployments
+    }
+
+    boolean timedOut(TimeDuration limit, Date startTime, Date currentTime) {
+        TimeCategory.minus(currentTime, startTime) > limit
+    }
+
+    private int getNumberOfDeployments(String marathonDeploymentUrl, String applicationId) {
+        def deploymentCount = 0
+        def result = asyncHttpClient.prepareGet(marathonDeploymentUrl).execute().get(3, TimeUnit.SECONDS)
+        def parsedDeployments = jsonSlurper.parse(result.getResponseBodyAsBytes())
+        parsedDeployments.each { it.affectedApps.each { app -> if (app == "/$applicationId") { deploymentCount++ } } }
+        return deploymentCount
     }
 }
--- src/test/groovy/cz/alenkacz/gradle/marathon/deploy/DeployTaskTest.groovy
+++ src/test/groovy/cz/alenkacz/gradle/marathon/deploy/DeployTaskTest.groovy
@@ -1,5 +1,6 @@
 package cz.alenkacz.gradle.marathon.deploy
 
+import groovy.time.TimeDuration
 import mesosphere.marathon.client.Marathon
 import mesosphere.marathon.client.MarathonClient
 import org.gradle.testfixtures.ProjectBuilder
@@ -48,11 +49,9 @@ class DeployTaskTest extends Specification  {
         def project = ProjectBuilder.builder().build()
         project.plugins.apply 'cz.alenkacz.gradle.marathon.deploy'
         def extension = (PluginExtension) project.extensions.findByName('marathon')
-        def marathonHost = System.getenv("MARATHON_HOST")
-        def marathonPort = System.getenv("MARATHON_TCP_8080")
-        def marathonUrl = "http://$marathonHost:$marathonPort"
+        def marathonUrl = getMarathonUrl()
         extension.setUrl(marathonUrl)
-        extension.setPathToJsonFile("src/test/resources/marathon.json")
+        extension.setPathToJsonFile(validMarathonJsonPath())
 
         when:
         project.tasks.deployToMarathon.deployToMarathon()
@@ -60,11 +59,90 @@ class DeployTaskTest extends Specification  {
         then:
         noExceptionThrown()
         applicationIsDeployed("testcontainer", marathonUrl)
+    }
 
+    def "fail when incorrect marathon url provided"() {
+        given:
+        def project = ProjectBuilder.builder().build()
+        project.plugins.apply 'cz.alenkacz.gradle.marathon.deploy'
+        def extension = (PluginExtension) project.extensions.findByName('marathon')
+        extension.setUrl("http://nonexistingmarathonurl")
+        extension.setPathToJsonFile(validMarathonJsonPath())
+
+        when:
+        project.tasks.deployToMarathon.deployToMarathon()
+
+        then:
+        Exception ex = thrown()
+        ex in MarathonDeployerException
+    }
+
+    def "fail when container cannot be deployed and verification times out"() {
+        given:
+        def project = ProjectBuilder.builder().build()
+        project.plugins.apply 'cz.alenkacz.gradle.marathon.deploy'
+        def extension = (PluginExtension) project.extensions.findByName('marathon')
+        extension.setUrl(getMarathonUrl())
+        extension.setPathToJsonFile(invalidMarathonJsonPath())
+        extension.verificationTimeout = new TimeDuration(0, 0, 5, 0)
+
+        when:
+        project.tasks.deployToMarathon.deployToMarathon()
+
+        then:
+        Exception ex = thrown()
+        ex in MarathonDeployerException
+        ex.printStackTrace()
+        ex.message.toLowerCase().contains("unable to verify deployment")
+    }
+
+    private def String getMarathonUrl() {
+        def marathonHost = System.getenv("MARATHON_HOST")
+        def marathonPort = System.getenv("MARATHON_TCP_8080")
+        return "http://$marathonHost:$marathonPort"
     }
 
     private def applicationIsDeployed(String name, String marathonUrl) {
         def marathon = MarathonClient.getInstance(marathonUrl)
         marathon.getApp(name) // throws exception "Not Found (http status: 404)" when not exists
+    }
+
+    private def validMarathonJsonPath() {
+        def marathonFilePath = ""
+        File.createTempFile("marathon",".json").with {
+            deleteOnExit()
+
+            write """{
+  "id": "testcontainer",
+  "cpus": 1,
+  "mem": 128,
+  "instances": 1,
+  "cmd": "while [ true ] ; do echo 'Hello Marathon' ; sleep 5 ; done"
+}"""
+            marathonFilePath = absolutePath
+        }
+        return marathonFilePath
+    }
+
+    private def invalidMarathonJsonPath() {
+        def marathonFilePath = ""
+        File.createTempFile("marathon",".json").with {
+            deleteOnExit()
+
+            write """{
+  "id": "testcontainerinvalid",
+  "cpus": 1,
+  "mem": 128,
+  "instances": 1,
+  "container": {
+        "type": "DOCKER",
+        "docker": {
+            "image": "nonexisting/image"
+        }
+   }
+}"""
+            marathonFilePath = absolutePath
+        }
+        return marathonFilePath
     }
 }
