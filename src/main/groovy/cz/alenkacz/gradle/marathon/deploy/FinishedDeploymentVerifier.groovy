c637e75b68e78a679d5f6f78251dd8e53c54490c
Alena Varkockova <varkockova.a@gmail.com>
2016-12-20 22:02:35 +0100

    Use jersey for events parsing

--- src/main/groovy/cz/alenkacz/gradle/marathon/deploy/FinishedDeploymentVerifier.groovy
+++ src/main/groovy/cz/alenkacz/gradle/marathon/deploy/FinishedDeploymentVerifier.groovy
@@ -5,8 +5,15 @@ import groovy.json.JsonSlurper
 import groovy.time.TimeCategory
 import groovy.time.TimeDuration
 import org.asynchttpclient.*
+import org.glassfish.jersey.media.sse.EventSource
+import org.glassfish.jersey.media.sse.InboundEvent
+import org.glassfish.jersey.media.sse.SseFeature
 import org.gradle.api.logging.Logger
+import org.glassfish.jersey.media.sse.EventListener
 
+import javax.ws.rs.client.Client
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.WebTarget
 import java.util.concurrent.ConcurrentHashMap
 
 class FinishedDeploymentVerifier {
@@ -16,8 +23,11 @@ class FinishedDeploymentVerifier {
     private JsonSlurper jsonSlurper
     private ConcurrentHashMap<String, Boolean> finishedDeployments
     private ListenableFuture<String> eventStreamFuture
+    private EventSource eventSource
+    private Client client
 
-    public FinishedDeploymentVerifier(AsyncHttpClient asyncHttpClient, JsonSlurper jsonSlurper, String marathonApiUrl, Logger logger) {
+    public FinishedDeploymentVerifier(Client client, AsyncHttpClient asyncHttpClient, JsonSlurper jsonSlurper, String marathonApiUrl, Logger logger) {
+        this.client = client
         this.jsonSlurper = jsonSlurper
         this.logger = logger
         this.marathonApiUrl = marathonApiUrl
@@ -26,52 +36,23 @@ class FinishedDeploymentVerifier {
     }
 
     public void startCapturingFinishedDeployments() {
-        eventStreamFuture = asyncHttpClient.prepareGet("$marathonApiUrl/events").addHeader("Accept", "text/event-stream").execute(new AsyncHandler<String>() {
-
-            @Override
-            void onThrowable(Throwable t) {
-                logger.error("Exception thrown when reading Marathon event stream, trying to make sure that deployment was finished", t)
-            }
-
-            // Example of event that we are interested in:
-            // event: deployment_success
-            // data: {"id":"bc7b0fbd-a9ad-4045-85bd-41fbad569abe","plan":{"id":"bc7b0fbd-a9ad-4045-85bd-41fbad569abe","original":{"id":"/","apps":[{"id":"/testcontainer-canary","cmd":"while [ true ] ; do echo 'Hello Marathon' ; sleep 5 ; done","args":null,"user":null,"env":{},"instances":1,"cpus":1,"mem":128,"disk":0,"executor":"","constraints":[],"uris":[],"fetch":[],"storeUrls":[],"ports":[10000],"portDefinitions":[{"port":10000,"protocol":"tcp","labels":{}}],"requirePorts":false,"backoffSeconds":1,"backoffFactor":1.15,"maxLaunchDelaySeconds":3600,"container":null,"healthChecks":[],"readinessChecks":[],"dependencies":[],"upgradeStrategy":{"minimumHealthCapacity":1,"maximumOverCapacity":1},"labels":{},"acceptedResourceRoles":null,"ipAddress":null,"version":"2016-09-04T17:48:40.394Z","residency":null,"versionInfo":{"lastScalingAt":"2016-09-04T17:48:40.394Z","lastConfigChangeAt":"2016-09-04T17:48:40.394Z"}}],"groups":[],"dependencies":[],"version":"2016-09-04T17:48:40.394Z"},"target":{"id":"/","apps":[{"id":"/testcontainer-canary","cmd":"while [ true ] ; do echo 'Hello Marathon' ; sleep 5 ; done","args":null,"user":null,"env":{},"instances":1,"cpus":1,"mem":128,"disk":0,"executor":"","constraints":[],"uris":[],"fetch":[],"storeUrls":[],"ports":[10000],"portDefinitions":[{"port":10000,"protocol":"tcp","labels":{}}],"requirePorts":false,"backoffSeconds":1,"backoffFactor":1.15,"maxLaunchDelaySeconds":3600,"container":null,"healthChecks":[],"readinessChecks":[],"dependencies":[],"upgradeStrategy":{"minimumHealthCapacity":1,"maximumOverCapacity":1},"labels":{},"acceptedResourceRoles":null,"ipAddress":null,"version":"2016-09-04T17:48:40.394Z","residency":null,"versionInfo":{"lastScalingAt":"2016-09-04T17:48:40.394Z","lastConfigChangeAt":"2016-09-04T17:48:40.394Z"}},{"id":"/testcontainer","cmd":"while [ true ] ; do echo 'Hello Marathon' ; sleep 5 ; done","args":null,"user":null,"env":{},"instances":1,"cpus":1,"mem":128,"disk":0,"executor":"","constraints":[],"uris":[],"fetch":[],"storeUrls":[],"ports":[10001],"portDefinitions":[{"port":10001,"protocol":"tcp","labels":{}}],"requirePorts":false,"backoffSeconds":1,"backoffFactor":1.15,"maxLaunchDelaySeconds":3600,"container":null,"healthChecks":[],"readinessChecks":[],"dependencies":[],"upgradeStrategy":{"minimumHealthCapacity":1,"maximumOverCapacity":1},"labels":{},"acceptedResourceRoles":null,"ipAddress":null,"version":"2016-09-04T17:48:46.846Z","residency":null,"versionInfo":{"lastScalingAt":"2016-09-04T17:48:46.846Z","lastConfigChangeAt":"2016-09-04T17:48:46.846Z"}}],"groups":[],"dependencies":[],"version":"2016-09-04T17:48:46.846Z"},"steps":[{"actions":[{"type":"StartApplication","app":"/testcontainer"}]},{"actions":[{"type":"ScaleApplication","app":"/testcontainer"}]}],"version":"2016-09-04T17:48:46.846Z"},"eventType":"deployment_success","timestamp":"2016-09-04T17:48:50.775Z"}
-            @Override
-            AsyncHandler.State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
-                def receivedEvent = new String(bodyPart.getBodyPartBytes(), "UTF-8")
-                if (receivedEvent.startsWith("event: deployment_success")) {
-                    logger.debug("Found deployment_success event in the marathon event stream")
-                    try {
-                        def foundDeploymentId = jsonSlurper.parse(receivedEvent.split("data: ")[1].getBytes()).id
-                        finishedDeployments.put(foundDeploymentId, true)
-                        logger.debug("New finished deployment of id $foundDeploymentId")
-                    } catch (JsonException e) {
-                        logger.warn("Unexpected format of event in Marathon event stream: $receivedEvent", e)
-                    }
+        WebTarget target = client.target("$marathonApiUrl/events");
+        eventSource = EventSource.target(target).build();
+        EventListener listener = new EventListener() {
+            public void onEvent(InboundEvent inboundEvent) {
+                def receivedEvent = inboundEvent.readData()
+                logger.debug("Found deployment_success event in the marathon event stream")
+                try {
+                    def foundDeploymentId = jsonSlurper.parse(receivedEvent.getBytes()).id
+                    finishedDeployments.put(foundDeploymentId, true)
+                    logger.debug("New finished deployment of id $foundDeploymentId")
+                } catch (JsonException e) {
+                    logger.warn("Unexpected format of event in Marathon event stream: $receivedEvent", e)
                 }
-                logger.debug("Received new event from Marathon event stream, but it was not interesting one for deployment verification")
-                return AsyncHandler.State.CONTINUE
             }
-
-            @Override
-            AsyncHandler.State onStatusReceived(HttpResponseStatus responseStatus) throws Exception {
-                def statusCode = responseStatus.getStatusCode()
-                if (statusCode < 200 || statusCode >= 400) {
-                    return AsyncHandler.State.ABORT
-                }
-                return AsyncHandler.State.CONTINUE
-            }
-
-            @Override
-            AsyncHandler.State onHeadersReceived(HttpResponseHeaders headers) throws Exception {
-                return AsyncHandler.State.CONTINUE
-            }
-
-            @Override
-            String onCompleted() throws Exception {
-                return ""
-            }
-        })
+        };
+        eventSource.register(listener, "deployment_success")
+        eventSource.open();
     }
 
     public boolean isDeploymentFinished(String deploymentId, TimeDuration timeout) {
@@ -97,6 +78,7 @@ class FinishedDeploymentVerifier {
     public void close() {
         try {
             eventStreamFuture.done()
+            eventSource.close()
         } catch (Exception ignored) {
             // ignoring
         }
