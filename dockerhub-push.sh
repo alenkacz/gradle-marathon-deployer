24cc1ee8ddee548f71a83db225e5f721e7091d8e
Alena Varkockova <varkockova.a@gmail.com>
2017-05-24 23:13:41 +0200

    Proper tag handling in dockerhub push

--- dockerhub-push.sh
+++ dockerhub-push.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -H "Content-Type: application/json" --data '{"docker_tag": "$TRAVIS_TAG"}' -X POST https://registry.hub.docker.com/u/alenkacz/marathon-deployer/trigger/$DOCKER_HUB_TOKEN/
+curl -H "Content-Type: application/json" --data  '{"source_type": "Tag", "source_name": "$TRAVIS_TAG"}' -X POST https://registry.hub.docker.com/u/alenkacz/marathon-deployer/trigger/$DOCKER_HUB_TOKEN/
