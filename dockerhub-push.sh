110341629e27493ff408235fb9da506ad7256267
Alena Varkockova <varkockova.a@gmail.com>
2017-05-25 09:35:24 +0200

    Missing docker build argument

--- dockerhub-push.sh
+++ dockerhub-push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd docker-distribution
-docker build --build-arg PLUGIN_VERSION="$TRAVIS_TAG" -t "alenkacz/marathon-deployer:$TRAVIS_TAG"
+docker build --build-arg PLUGIN_VERSION="$TRAVIS_TAG" -t "alenkacz/marathon-deployer:$TRAVIS_TAG" .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push "alenkacz/marathon-deployer:$TRAVIS_TAG"
