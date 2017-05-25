6b6e49f7e42ad31d9f3b8f0746c110d228718cbd
Alena Varkockova <varkockova.a@gmail.com>
2017-05-25 10:40:44 +0200

    Use sudo when working with docker

--- dockerhub-push.sh
+++ dockerhub-push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd docker-distribution
-docker build --build-arg PLUGIN_VERSION="$TRAVIS_TAG" -t "alenkacz/marathon-deployer:$TRAVIS_TAG" .
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push "alenkacz/marathon-deployer:$TRAVIS_TAG"
+sudo docker build --build-arg PLUGIN_VERSION="$TRAVIS_TAG" -t "alenkacz/marathon-deployer:$TRAVIS_TAG" .
+sudo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+sudo docker push "alenkacz/marathon-deployer:$TRAVIS_TAG"
