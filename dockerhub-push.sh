7aecc4b67816b4eb5f06a2ecf22317980501bd9c
Alena Varkockova <varkockova.a@gmail.com>
2017-05-25 09:04:57 +0200

    Push directly to docker hub
    no docker hub automated builds

--- dockerhub-push.sh
+++ dockerhub-push.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl -H "Content-Type: application/json" --data  '{"source_type": "Tag", "source_name": "$TRAVIS_TAG"}' -X POST https://registry.hub.docker.com/u/alenkacz/marathon-deployer/trigger/$DOCKER_HUB_TOKEN/
+cd docker-distribution
+docker build --build-arg PLUGIN_VERSION="$TRAVIS_TAG" -t "alenkacz/marathon-deployer:$TRAVIS_TAG"
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push "alenkacz/marathon-deployer:$TRAVIS_TAG"
